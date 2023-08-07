b2b5160eb70bc16829b23b4ab22130e024923d42
oAntonioMauricio <oantoniomauricio@gmail.com>
2023-08-07 09:55:08 +0100

    add save_game branch

--- src/pt/upskill/projeto1/game/Engine.java
+++ src/pt/upskill/projeto1/game/Engine.java
@@ -29,7 +29,7 @@ public class Engine {
         engine.init();
     }
 
-    // MAIN BRANCH
+    // save_game branch
     //
     // PERGUNTA: Statusbar no singleton ou como atributo do heroi?
     //
